730b2b5e36f11ab312791b947bf4b9dcfff6db8e
Naugtur <naugtur@gmail.com>
2014-12-14 15:27:17 +0100

    topic hierarchy

--- index.js
+++ index.js
@@ -14,14 +14,11 @@ function clone(obj) {
     }
 }
 
-
-function topicHandler(topic) {
-    var t = topic.split('#');
-    return {
-        to: t[0],
-        hash: t[1]
-    }
+function getByTopic(collection, topic) {
+    var parentTopic = topic.replace(/\.[^.]+$/, '');
+    return [].concat(collection[topic], (topic !== parentTopic ? getByTopic(collection, parentTopic) : []));
 }
+
 
 
 function init() {
@@ -33,53 +30,40 @@ function init() {
 
     //subscribes a function to a topic
     function subscribe(topic, func) {
-        var t = topicHandler(topic);
-        if (!subscriptions[t.to]) {
-            subscriptions[t.to] = [];
+        if (!subscriptions[topic]) {
+            subscriptions[topic] = [];
         }
-        subscriptions[t.to].push({
-            hash: t.hash,
+        subscriptions[topic].push({
             func: func
         });
     }
 
     //clears the topic
-    function unsubscribe(topic) {
-        var t = topicHandler(topic);
-        if (!t.hash) {
-            if (subscriptions[t.to] && (subscriptions[t.to].length > 0)) {
-                subscriptions[t.to] = [];
-            }
-        } else {
-            subscriptions[t.to] = subscriptions[t.to].filter(function (s) {
-                return s.hash !== t.hash;
-            });
+    function unsubscribeAll(topic) {
+        if (subscriptions[topic]) {
+            subscriptions[topic] = [];
         }
     }
 
     //publishes in topic
     function publish(topic, input) {
-        var t = topicHandler(topic);
+
+        var selectedFilters = getByTopic(filters, topic);
+        var selectedSubs = getByTopic(subscriptions, topic);
 
         return q(true).then(function () {
             var data = (input) ? clone(input) : {};
             var promiseArgs = q(data);
-            if (filters[t.to] && filters[t.to].length > 0) {
-                filters[t.to].forEach(function (args, filter) {
-                    promiseArgs = promiseArgs.then(filter);
-                });
-            }
+            selectedFilters.forEach(function (args, filter) {
+                promiseArgs = promiseArgs.then(filter);
+            });
             return promiseArgs;
         }).then(function (data) {
             var todos = [];
-            if (subscriptions[t.to] && subscriptions[t.to].length > 0) {
-                //call subscribers
-                todos = todos.concat(subscriptions[t.to].filter(function (subber) {
-                    return (!subber.hash || subber.hash === t.hash);
-                }).map(function (subber) {
-                    return subber.func(data);
-                }));
-            }
+            //call subscribers
+            todos = todos.concat(selectedSubs.map(function (subber) {
+                return subber.func(data);
+            }));
             for (var ch in forwards) {
                 //TODO, this is kinda inconsequent, resolutions from there are going to be returned as arrays
                 //but at least error handling is ok.
@@ -132,7 +116,7 @@ function init() {
             }),
             subscriber: mkDef({
                 subscribe: subscribe,
-                unsubscribe: unsubscribe,
+                unsubscribeAll: unsubscribeAll,
                 promises: q
             }),
             filter: mkDef({
